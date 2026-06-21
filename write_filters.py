08467291868a9bcaba499c37112eb6de2a36f5f5
italostatonato <italo.statonato@gmail.com>
2026-06-21 19:51:24 -0300

    chore: remove scripts temporarios de debug (filters_content.txt, write_filters.py)

--- write_filters.py
+++
@@ -1,4 +0,0 @@
-content = open("filters_content.txt", "r", encoding="utf-8").read()
-with open("src/filters.py", "w", encoding="utf-8") as f:
-    f.write(content)
-print("filters.py reescrito com sucesso, encoding UTF-8 garantido.")
